f53446581ec1836576572d49eca6f848a70eeb31
Patrick Frampton <patrick.frampton@gmail.com>
2015-10-24 15:26:41 -0700

    Clean up tests

--- test/TodoApi.Tests/TodoControllerTests.cs
+++ test/TodoApi.Tests/TodoControllerTests.cs
@@ -1,46 +1,145 @@
 using Xunit;
 using TodoApi.Controllers;
 using TodoApi.Models;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc;
+using Xunit.Abstractions;
 
 namespace TodoApi.Tests
 {	
 	public class TodoControllerTests
 	{
-		[Fact]
-		public  void GetAllGetsATodo()
+		private readonly ITestOutputHelper output;
+		
+		public TodoControllerTests(ITestOutputHelper output)
 		{
-			var controller = new TodoController();
-			
-			// this would normally have to be mocked or something, but 
-			// it's just a dictionary anyway. We can use it directly.
-			// Under normal circumstances this is populated via Service injection
-			controller.TodoItems = new TodoRepository();
-			
-			var result = controller.GetAll();
-			
-			int counter = 0;
-			foreach (var item in result) {
-				counter++;
-				Assert.NotNull(item.Key);         // key is *something*
-				Assert.False(item.IsComplete);    // not complete
-				Assert.Equal("Item1", item.Name);  // Default item 
+			this.output = output;
+		}
+		public class LocalTodoRepository : ITodoRepository
+		{
+			private Dictionary<string, TodoItem> _lookup;
+			private int nextID;
+			public LocalTodoRepository() 
+			{
+				 _lookup = new Dictionary<string, TodoItem>();
+				 nextID = 1;
 			}
-			Assert.Equal(1, counter);
+			
+			public IEnumerable<TodoItem> GetAll() 
+			{
+				return _lookup.Values;
+			}
+			
+			public TodoItem Find(string key)
+			{
+				TodoItem item;
+				_lookup.TryGetValue(key, out item);
+				return item;
+			}
+			
+			public void Add(TodoItem item)
+			{
+				item.Key = nextID.ToString();
+				nextID++;
+				_lookup.Add(item.Key, item);
+			}
+			
+			public TodoItem Remove(string key)
+			{
+				TodoItem item;
+				_lookup.TryGetValue(key, out item);
+				if (item != null) {
+					_lookup.Remove(key);
+				}
+				return item;
+			}
+			
+			public void Update(TodoItem item)
+			{
+				_lookup[item.Key] = item;
+			}
+			
+		}
+		
+		public TodoController GetController(ITodoRepository repo) {
+			var controller = new TodoController();
+			controller.TodoItems = repo;
+			
+			return controller;
+		}
+		
+		
+		[Fact]
+		public  void GetAllFromEmptyReturnsNothing()
+		{
+			var controller = GetController(new LocalTodoRepository());
+			var result = controller.GetAll() as ICollection<TodoItem>;
+			
+			Assert.Equal(0, result.Count);
 		}
 		
 		[Fact]
-		public void TestGet() 
+		public  void GetAllShouldReturnEverything()
 		{
-			var controller = new TodoController();
-			controller.TodoItems = new TodoRepository();
+			var repo = new LocalTodoRepository();
 			
-			var result = controller.GetAll();
-			TodoItem item = result.GetEnumerator().Current;
+			repo.Add(new TodoItem() { Name = "Test", IsComplete=false });
+			repo.Add(new TodoItem() { Name= "Code", IsComplete=true });
 			
-			Assert.NotNull(item);
-			//var result = controller.GetById(item.Key);
+			var controller = GetController(repo);
 			
+			var result = controller.GetAll() as ICollection<TodoItem>;
+			
+			Assert.Equal(2, result.Count);
+			
+			bool testFound = false;
+			bool codeFound = false;
+			
+			foreach (var item in result) {
+				if (item.Name == "Test") {
+					testFound = true;
+					Assert.Equal(false, item.IsComplete);
+					Assert.Equal("1", item.Key);
+				} else if (item.Name == "Code" ) {
+					codeFound = true;
+					Assert.Equal(true, item.IsComplete);
+					Assert.Equal("2", item.Key);
+				}
+			}
+			
+			Assert.True(testFound);
+			Assert.True(codeFound);
 		}
+		
+
+/*		
+		[Fact]
+		public void TestGet() 
+		{	
+			var repo = new LocalTodoRepository();
+			repo.Add(new TodoItem() { Name="FindMe", IsComplete=false});
+			var controller = GetController(repo);
+			
+			
+			// Make sure we DON'T get back the result.
+		 	var result = controller.GetById("7") as HttpNotFoundObjectResult;
+			
+			Assert.NotNull(result); 
+			result.ExecuteResult(new ActionContext());
+			
+			Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+			
+			
+			var goodResult = controller.GetById("1") as ObjectResult;
+			goodResult.ExecuteResult(new ActionContext());
+			Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+			TodoItem item = goodResult.Value as TodoItem;
+			Assert.Equal("1", item.Key);
+			Assert.Equal("FindMe", item.Name);
+			Assert.False(item.IsComplete);	
+		}
+		*/
 	}
 }
