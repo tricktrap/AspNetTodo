b1536e461d342989e8360f8b400bf05e5c46737d
Patrick Frampton <patrick.frampton@gmail.com>
2015-10-24 11:52:31 -0700

    Add get methods

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -9,6 +9,24 @@ namespace TodoApi.Controllers
 	{
 		[FromServices]
 		public ITodoRepository TodoItems {get; set;}
+		
+		[HttpGet]
+		public IEnumerable<TodoItem> GetAll()
+		{
+			return TodoItems.GetAll();
+		}
+		
+		[HttpGet("{id}", Name = "GetTodo")]
+		public IActionResult GetById(string id)
+		{
+			var item = TodoItems.Find(id);
+			
+			if (item == null)
+			{
+				return HttpNotFound();
+			}
+			return new ObjectResult(item);
+		}
 	}
 	
 }
